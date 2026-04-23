14c9db570c884fefa0e2fbba6b6d9aa4a4cbfae5
Kaley <kinman@mcad.edu>
2026-04-23 17:19:06 -0500

    Add files via upload

--- week13/exercise13/vite.config.js
+++ week13/exercise13/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactassingnmenta1/week13/exercise13/",
+  base: "/reactassignments1/week13/exercise13/",
 })
+  
