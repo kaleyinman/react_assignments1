3aaa801c13df71ab184d7ae2d201a1fe36fb6312
Kaley <kinman@mcad.edu>
2026-04-30 18:08:50 -0500

    Add files via upload

---
+++ week14/assignment14/src/App.jsx
@@ -0,0 +1,14 @@
+import "./css/app.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <About />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ week14/exercise14/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+
+            <ul className="nav-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
