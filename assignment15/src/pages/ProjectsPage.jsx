3f50e852094c190e624ad3d15cc7a30dbe8d61f3
Kaley <kinman@mcad.edu>
2026-05-09 19:54:49 -0500

    Add files via upload

---
+++ assignment15/src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import Projects from "../components/Projects";
+
+function ProjectsPage() {
+  return (
+    <div>
+        <h1>Projects Page</h1>
+        <Projects />
+    </div>
+  );
+}
+
+export default ProjectsPage;
